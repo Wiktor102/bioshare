bfffbcaa20c741fe12d9a175c741e3244c0821b6
Wiktor102 <wiktorgolicz1@gmail.com>
2023-05-25 21:26:04 +0200

    Basic login screen

---
+++ app/bioshare/lib/login.dart
@@ -0,0 +1,242 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textEditingController.clear();
+    super.dispose();
+  }
+
+  onEmailChanged(val) {
+    setState(() {
+      // isEmailCorrect = isEmail(val);
+      isEmailCorrect = true;
+    });
+  }
+
+  bool isEmailCorrect = false;
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 350 - 40,
+            height: MediaQuery.of(context).size.height - 350 + 40,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  opacity: 0.3,
+                  image: AssetImage('assets/fridgeBG.jpg'),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            height: 350,
+            width: MediaQuery.of(context).size.width,
+            child: RotationTransition(
+              turns: const AlwaysStoppedAnimation(180 / 360),
+              child: ClipPath(
+                clipper: Clipper(),
+                child: Container(
+                  //   alignment: Alignment.bottomCenter,
+                  decoration:
+                      BoxDecoration(color: Theme.of(context).primaryColorLight),
+                ),
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    //   Image.asset(
+                    //     'assets/images/logo_new.png',
+                    //     height: 120,
+                    //     width: 120,
+                    //   ),
+                    const Text(
+                      'Zaloguj się',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        'By korzystać z naszej aplikacji musisz być zalogowany',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.w300,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width / 1.1,
+                      margin: const EdgeInsets.symmetric(vertical: 30),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: _textEditingController,
+                              onChanged: onEmailChanged,
+                              decoration: CustomInputDecoration(
+                                context,
+                                labelText: "Email",
+                                hintText: 'Wpisz swój adres e-mail',
+                                prefixIcon: Icon(
+                                  Icons.person,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            TextFormField(
+                              obscureText: true,
+                              obscuringCharacter: '*',
+                              decoration: CustomInputDecoration(
+                                context,
+                                labelText: "Password",
+                                hintText: 'Wpisz swoje hasło',
+                                prefixIcon: Icon(
+                                  Icons.lock,
+                                  color: Theme.of(context).primaryColor,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                    ),
+                                    onPressed: () {
+                                      if (_formKey.currentState!.validate()) {
+                                        ScaffoldMessenger.of(context)
+                                            .showSnackBar(
+                                          const SnackBar(
+                                              content: Text('Processing Data')),
+                                        );
+                                      }
+                                    },
+                                    child: const Text(
+                                      'Kontynuuj',
+                                      style: TextStyle(fontSize: 17),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Nie masz jeszcze konta?',
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.6),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(
+                            'Załóż je',
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomInputDecoration extends InputDecoration {
+  CustomInputDecoration(context,
+      {Icon? prefixIcon, String? labelText, String? hintText})
+      : super(
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            enabledBorder: const UnderlineInputBorder(
+              borderSide: BorderSide.none,
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+            filled: true,
+            fillColor: Colors.white,
+            labelStyle: TextStyle(
+              color: Theme.of(context).primaryColorDark.withOpacity(0.7),
+            ),
+            prefixIcon: prefixIcon,
+            labelText: labelText,
+            hintText: hintText);
+}
+
+class Clipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height);
+    path.quadraticBezierTo(
+        size.width / 4, size.height - 40, size.width / 2, size.height - 20);
+    path.quadraticBezierTo(
+        3 / 4 * size.width, size.height, size.width, size.height - 30);
+    path.lineTo(size.width, 0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(Clipper oldClipper) => false;
+}
--- app/bioshare/lib/main.dart
+++ app/bioshare/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import "./bottom_nav.dart";
+import "./login.dart";
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const App(),
+      home: const LoginPage(),
     );
   }
 }
