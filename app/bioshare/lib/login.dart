7d7a77630fef5b33f8bcf13c5dac3a728f539e13
Wiktor102 <wiktorgolicz1@gmail.com>
2023-05-26 19:35:21 +0200

    Unfocus inputs on outside click

--- app/bioshare/lib/login.dart
+++ app/bioshare/lib/login.dart
@@ -30,166 +30,170 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            bottom: 350 - 40,
-            height: MediaQuery.of(context).size.height - 350 + 40,
-            width: MediaQuery.of(context).size.width,
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  opacity: 0.3,
-                  image: AssetImage('assets/fridgeBG.jpg'),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: 350 - 40,
+              height: MediaQuery.of(context).size.height - 350 + 40,
+              width: MediaQuery.of(context).size.width,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    opacity: 0.3,
+                    image: AssetImage('assets/fridgeBG.jpg'),
+                  ),
                 ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            height: 350,
-            width: MediaQuery.of(context).size.width,
-            child: RotationTransition(
-              turns: const AlwaysStoppedAnimation(180 / 360),
-              child: ClipPath(
-                clipper: Clipper(),
-                child: Container(
-                  //   alignment: Alignment.bottomCenter,
-                  decoration:
-                      BoxDecoration(color: Theme.of(context).primaryColorLight),
+            Positioned(
+              bottom: 0,
+              height: 350,
+              width: MediaQuery.of(context).size.width,
+              child: RotationTransition(
+                turns: const AlwaysStoppedAnimation(180 / 360),
+                child: ClipPath(
+                  clipper: Clipper(),
+                  child: Container(
+                    //   alignment: Alignment.bottomCenter,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColorLight),
+                  ),
                 ),
               ),
             ),
-          ),
-          SafeArea(
-            child: Center(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Zaloguj się',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40,
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 10, right: 10, top: 5),
-                      child: Text(
-                        'By korzystać z naszej aplikacji, musisz być zalogowany',
-                        textAlign: TextAlign.center,
+            SafeArea(
+              child: Center(
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Zaloguj się',
                         style: TextStyle(
-                          color: Colors.black.withOpacity(0.7),
-                          fontWeight: FontWeight.w300,
-                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
                         ),
                       ),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 1.1,
-                      margin: const EdgeInsets.symmetric(vertical: 30),
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(20),
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 10, right: 10, top: 5),
+                        child: Text(
+                          'By korzystać z naszej aplikacji, musisz być zalogowany',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black.withOpacity(0.7),
+                            fontWeight: FontWeight.w300,
+                            fontSize: 15,
+                          ),
+                        ),
                       ),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            TextFormField(
-                              controller: _textEditingController,
-                              onChanged: onEmailChanged,
-                              decoration: CustomInputDecoration(
-                                context,
-                                labelText: "E-mail",
-                                hintText: 'Wpisz swój adres e-mail',
-                                prefixIcon: Icon(
-                                  Icons.email,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextFormField(
-                              obscureText: true,
-                              obscuringCharacter: '*',
-                              decoration: CustomInputDecoration(
-                                context,
-                                labelText: "Hasło",
-                                hintText: 'Wpisz swoje hasło',
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      if (_formKey.currentState!.validate()) {
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(
-                                          const SnackBar(
-                                              content: Text('Processing Data')),
-                                        );
-                                      }
-                                    },
-                                    child: const Text(
-                                      'Kontynuuj',
-                                      style: TextStyle(fontSize: 17),
-                                    ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.1,
+                        margin: const EdgeInsets.symmetric(vertical: 30),
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(0.3),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                controller: _textEditingController,
+                                onChanged: onEmailChanged,
+                                decoration: CustomInputDecoration(
+                                  context,
+                                  labelText: "E-mail",
+                                  hintText: 'Wpisz swój adres e-mail',
+                                  prefixIcon: Icon(
+                                    Icons.email,
+                                    color: Theme.of(context).primaryColor,
                                   ),
                                 ),
-                              ],
-                            )
-                          ],
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              TextFormField(
+                                obscureText: true,
+                                obscuringCharacter: '*',
+                                decoration: CustomInputDecoration(
+                                  context,
+                                  labelText: "Hasło",
+                                  hintText: 'Wpisz swoje hasło',
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Theme.of(context).primaryColor,
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 20,
+                              ),
+                              Row(
+                                children: [
+                                  Expanded(
+                                    child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        if (_formKey.currentState!.validate()) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(
+                                            const SnackBar(
+                                                content:
+                                                    Text('Processing Data')),
+                                          );
+                                        }
+                                      },
+                                      child: const Text(
+                                        'Kontynuuj',
+                                        style: TextStyle(fontSize: 17),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Nie masz jeszcze konta?',
-                          style: TextStyle(
-                            color: Colors.black.withOpacity(0.6),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Nie masz jeszcze konta?',
+                            style: TextStyle(
+                              color: Colors.black.withOpacity(0.6),
+                            ),
                           ),
-                        ),
-                        TextButton(
-                          onPressed: widget.goToSignup,
-                          child: const Text(
-                            'Załóż je',
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),
-                        )
-                      ],
-                    ),
-                  ],
+                          TextButton(
+                            onPressed: widget.goToSignup,
+                            child: const Text(
+                              'Załóż je',
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
