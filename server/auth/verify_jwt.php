04c2b3c1e9ab91421a06810ff5a76505da4e15e8
Wiktor102 <wiktorgolicz1@gmail.com>
2023-06-10 16:14:04 +0200

    Fix production server not working

--- server/auth/verify_jwt.php
+++ server/auth/verify_jwt.php
@@ -6,13 +6,12 @@ require_once realpath(dirname(__FILE__) . "/../vendor/autoload.php");
 function verifyJWT()
 {
 	$headers = getallheaders();
-	// print_r($headers);
-	if (!array_key_exists("authorization", $headers)) {
+	if (!array_key_exists("Authorization", $headers)) {
 		http_response_code(401);
 		exit();
 	}
 
-	$jwt = $headers["authorization"];
+	$jwt = $headers["Authorization"];
 	$jwt = str_replace("Bearer ", "", $jwt);
 	$jwtSecretKey = file_get_contents(realpath(dirname(__FILE__) . "/.jwt-secret"));
 
